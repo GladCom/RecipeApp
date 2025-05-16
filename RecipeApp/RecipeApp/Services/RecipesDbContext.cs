f4bf31ca6e742ab8ba5174c76d7b66085f58b66f
Dezodemius <gladkovyegor@gmail.com>
2025-05-16 23:11:40 +0400

    Создал шаблонный проект для Blazor Web App Server

---
+++ RecipeApp/RecipeApp/Services/RecipesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeApp.Model;
+
+namespace RecipeApp.Services;
+
+public class RecipesDbContext(DbContextOptions<RecipesDbContext> options) 
+  : DbContext(options)
+{
+  public DbSet<Recipe> Recipes => Set<Recipe>();
+
+  public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+}
