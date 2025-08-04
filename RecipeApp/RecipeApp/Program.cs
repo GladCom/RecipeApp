d20411a9eb3493f95c9e60340090a9ceadc1ccca
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-08-04 21:03:03 +0400

    Зарегистрирован AuthService

--- RecipeApp/RecipeApp/Program.cs
+++ RecipeApp/RecipeApp/Program.cs
@@ -7,45 +7,47 @@ namespace RecipeApp;
 
 public class Program
 {
-  public static void Main(string[] args)
-  {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Services.AddRazorComponents()
-      .AddInteractiveServerComponents();
-    builder.Services.AddDbContext<RecipesDbContext>(options =>
-      options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-    builder.Services.Configure<FormOptions>(options => {
-      options.MultipartBodyLengthLimit = 10 * 1024 * 1024;
-    });
-    builder.Services.AddRazorPages();
-    builder.Services.AddServerSideBlazor();
-    builder.Services.AddScoped<RecipeService>();
-
-    var app = builder.Build();
-
-    using (var scope = app.Services.CreateScope())
+    public static void Main(string[] args)
     {
-      var db = scope.ServiceProvider.GetRequiredService<RecipesDbContext>();
-      db.Database.Migrate();
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+          .AddInteractiveServerComponents();
+        builder.Services.AddDbContext<RecipesDbContext>(options =>
+          options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.Configure<FormOptions>(options =>
+        {
+            options.MultipartBodyLengthLimit = 10 * 1024 * 1024;
+        });
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddScoped<RecipeService>();
+        builder.Services.AddScoped<AuthService>();
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<RecipesDbContext>();
+            db.Database.Migrate();
+        }
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.MapRazorComponents<App>()
+          .AddInteractiveServerRenderMode();
+
+        app.UseRouting();
+        app.UseAntiforgery();
+
+        app.Run();
     }
-
-    if (!app.Environment.IsDevelopment())
-    {
-      app.UseExceptionHandler("/Error");
-      app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseStaticFiles();
-
-    app.MapRazorComponents<App>()
-      .AddInteractiveServerRenderMode();
-
-    app.UseRouting();
-    app.UseAntiforgery();
-
-    app.Run();
-  }
 }
