b7826ebf6a81f8c772a519e6444fdd707b7ff474
Dezodemius <gladkovyegor@gmail.com>
2025-08-05 21:40:16 +0400

    fix in index.razor

--- RecipeApp/RecipeApp/Components/Index.razor.cs
+++ RecipeApp/RecipeApp/Components/Index.razor.cs
@@ -6,72 +6,121 @@ using RecipeApp.Model;
 
 namespace RecipeApp.Components;
 
+/// <summary>
+/// Кодовая часть страницы всех рецептов.
+/// </summary>
 public partial class Index
 {
+  #region Поля и свойства
+
+  /// <summary>
+  /// Количество элементов на одной странице.
+  /// </summary>
   private const int PageSize = 10;
 
-  private List<Recipe>? _recipes;
+  /// <summary>
+  /// Рецепты, которые будут отображены на странице.
+  /// </summary>
+  private List<Recipe>? recipes;
 
-  private string _searchText = "";
+  /// <summary>
+  /// Текущая страница.
+  /// </summary>
+  private int currentPage = 1;
 
-  private int _currentPage = 1;
+  private string searchText = string.Empty;
 
+  /// <summary>
+  /// Текст из поля для поиска.
+  /// </summary>
   private string SearchText
   {
     get
     {
-      return _searchText;
+      return this.searchText;
     }
+
     set
     {
-      if (_searchText != value)
+      if (this.searchText != value)
       {
-        _searchText = value;
-        _currentPage = 1;
+        this.searchText = value;
+        this.currentPage = 1;
       }
     }
   }
 
+  /// <summary>
+  /// Рецепты, которые найдены после поиска.
+  /// </summary>
   private IEnumerable<Recipe> FilteredRecipes
   {
     get
     {
-      return string.IsNullOrWhiteSpace(_searchText)
-        ? _recipes!
-        : _recipes!.Where(r =>
-          r.Title.Contains(_searchText, StringComparison.OrdinalIgnoreCase));
+      return string.IsNullOrWhiteSpace(this.searchText)
+        ? this.recipes!
+        : this.recipes!.Where(r =>
+          r.Title.Contains(this.searchText, StringComparison.OrdinalIgnoreCase));
     }
   }
 
+  /// <summary>
+  /// Общее количество страниц.
+  /// </summary>
   private int TotalPages =>
-    (int)Math.Ceiling((double)(FilteredRecipes?.Count() ?? 0) / PageSize);
+    (int)Math.Ceiling((double)(this.FilteredRecipes?.Count() ?? 0) / PageSize);
 
+  /// <summary>
+  /// Рецепты на одной странице.
+  /// </summary>
   private IEnumerable<Recipe> PagedRecipes =>
-    FilteredRecipes?
-      .Skip((_currentPage - 1) * PageSize)
+    this.FilteredRecipes
+      .Skip((this.currentPage - 1) * PageSize)
       .Take(PageSize)
     ?? [];
 
+  #endregion
+
+  #region Базовый класс
+
+  /// <inheritdoc />
   protected override Task OnInitializedAsync()
   {
-    _recipes = RecipeService.GetRecipes();
+    this.recipes = this.RecipeService.GetRecipes();
 
     return Task.CompletedTask;
   }
 
+  #endregion
+
+  #region Методы
+
+  /// <summary>
+  /// Добавить рецепт.
+  /// </summary>
   private void AddRecipe()
   {
-    NavigationManager.NavigateTo("/recipes/create");
+    this.NavigationManager.NavigateTo("/recipes/create");
   }
 
+  /// <summary>
+  /// Открыть рецепт.
+  /// </summary>
+  /// <param name="id">ИД рецепта, который нужно открыть.</param>
   private void ViewRecipe(int id)
   {
-    NavigationManager.NavigateTo($"/recipes/{id}");
+    this.NavigationManager.NavigateTo($"/recipes/{id}");
   }
 
+  /// <summary>
+  /// Перейти на указанную страницу.
+  /// </summary>
+  /// <param name="page">Указанная страница.</param>
   private void GoToPage(int page)
   {
-    if (page >= 1 && page <= TotalPages)
-      _currentPage = page;
+    if (page >= 1 && page <= this.TotalPages)
+      this.currentPage = page;
   }
+
+  #endregion
 }
