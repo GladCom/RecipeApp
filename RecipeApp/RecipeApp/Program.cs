b4cf2e885c4b368123f715fec23efff95174e9ea
Dezodemius <gladkovyegor@gmail.com>
2025-08-06 00:30:36 +0400

    test 2

--- RecipeApp/RecipeApp/Program.cs
+++ RecipeApp/RecipeApp/Program.cs
@@ -22,6 +22,7 @@ public static class Program
   {
     var builder = WebApplication.CreateBuilder(args);
     ;
+    ;
 
     builder.Services.AddRazorComponents()
       .AddInteractiveServerComponents();
