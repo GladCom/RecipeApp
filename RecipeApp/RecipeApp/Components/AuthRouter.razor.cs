30d4026833f1b550fea19a8d35d6de53f51c7cf8
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-08-06 19:24:01 +0400

    Перенесли код c# из AuthRouter

---
+++ RecipeApp/RecipeApp/Components/AuthRouter.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using RecipeApp.Services;
+
+namespace RecipeApp.Components;
+
+/// <summary>
+/// Компонент для маршрутизации на основе состояния аутентификации пользователя.
+/// Отображает либо основное содержимое приложения, либо модальное окно авторизации
+/// в зависимости от того, авторизован ли пользователь.
+/// </summary>
+public partial class AuthRouter : ComponentBase, IDisposable
+{
+    #region Методы
+
+    /// <summary>
+    /// Обработчик события изменения состояния аутентификации.
+    /// Вызывает перерисовку компонента при изменении состояния авторизации.
+    /// </summary>
+    /// <param name="sender">Источник события.</param>
+    /// <param name="e">Аргументы события.</param>
+    private void OnAuthStateChanged(object? sender, EventArgs e)
+    {
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// Вызывается при инициализации компонента.
+    /// Подписывается на событие изменения состояния аутентификации.
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        AuthService.AuthenticationStateChanged += OnAuthStateChanged;
+    }
+
+    /// <summary>
+    /// Освобождает ресурсы, используемые компонентом.
+    /// Отписывается от события изменения состояния аутентификации.
+    /// </summary>
+    public void Dispose()
+    {
+        AuthService.AuthenticationStateChanged -= OnAuthStateChanged;
+    }
+
+    #endregion
+}
