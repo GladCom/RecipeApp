9765562344fdfae3d27f7f82eceb50d6b2238426
Sveta Nikitina <srg.svt@gmail.com>
2025-08-10 23:10:47 +0400

    Update Ingredient.cs
    еще раз сортировка using

--- RecipeApp/RecipeApp/Model/Ingredient.cs
+++ RecipeApp/RecipeApp/Model/Ingredient.cs
@@ -1,6 +1,6 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace RecipeApp.Model;
 
